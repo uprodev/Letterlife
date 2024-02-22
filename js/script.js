829994cb6dde923f65bca16304bf3c879bc53c55
kvv1980 <ves1982@gmail.com>
2024-02-22 17:21:38 +0200

    add

--- js/script.js
+++ js/script.js
@@ -130,5 +130,60 @@ jQuery(document).ready(function ($) {
 
 
 
+  /*02-02-24*/
+  $(document).on('click', '.top-line .lang-wrap>a', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('is-open');
+    if($(this).hasClass('is-open')){
+      $(this).closest('.lang-wrap').find('ul').slideDown()
+    }else{
+      $(this).closest('.lang-wrap').find('ul').slideUp()
+    }
+  });
+
+
+
+  //22-02-24
+
+  //TIMER
+  simplyCountdown('.timer', {
+    year: 2024, // required
+    month: 6, // required
+    day: 28, // required
+    hours: 0, // Default is 0 [0-23] integer
+    minutes: 0, // Default is 0 [0-59] integer
+    seconds: 0, // Default is 0 [0-59] integer
+    words: { //words displayed into the countdown
+      days: { singular: 'Day', plural: 'Days' },
+      hours: { singular: 'Hour', plural: 'Hours' },
+      minutes: { singular: 'Minute', plural: 'Minutes' },
+      seconds: { singular: 'second', plural: 'seconds' }
+    },
+    plural: true, //use plurals
+    inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+    inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+    // in case of inline set to false
+    enableUtc: false, //Use UTC or not - default : false
+    onEnd: function() { return; }, //Callback on countdown end, put your own function here
+    refresh: 1000, // default refresh every 1s
+    sectionClass: 'simply-section', //section css class
+    amountClass: 'simply-amount', // amount css class
+    wordClass: 'simply-word', // word css class
+    zeroPad: false,
+    countUp: false
+  });
+
+  $(document).on('click', '.read-more', function (e){
+    e.preventDefault();
+    $(this).hide();
+    $('.banner-webinar .text-wrap p').slideDown();
+  });
+
+  $(document).on('click', '.mob-read', function (e){
+    e.preventDefault();
+    $(this).hide();
+    $(this).closest('.item').removeClass('item-hide-mob');
+  });
+
 
 });
